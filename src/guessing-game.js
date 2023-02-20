438117bd2e1aa5f060d7b7b93f7b2d99b672b8a3
Oleksandr <oleksandryurievich@gmail.com>
2023-02-20 16:36:59 +0200

    feat: create function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.minR = min;
+    this.maxR = max;
+  }
 
-    }
+  guess() {
+    return Math.round((this.maxR + this.minR) / 2);
+  }
 
-    guess() {
+  lower() {
+    this.maxR = Math.round((this.maxR + this.minR) / 2);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.minR = Math.round((this.maxR + this.minR) / 2);
+  }
 }
 
 module.exports = GuessingGame;
